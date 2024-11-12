8d3b5e53060afde0ab136da886a15dd9d5b02c02
Asikur Rahman <asikurbd25@gmail.com>
2024-11-12 17:27:43 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
-j
+function callAll(jsfiles) {
+    var src = document.createElement("script");
+    src.setAttribute("type", "text/javascript");
+    src.setAttribute("src", jsfiles);
+    document.getElementsByTagName("head")[0].appendChild(src);
+}
+callAll("/js/include.js");
